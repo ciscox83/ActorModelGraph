2d8e0f7a58cf63bbd6fad4ab07f6fb115c29fa4b
Christian Sisti <christian.sisti@ocado.com>
2015-07-06 22:42:12 +0100

    Mark artifact with scala 2.10 version. Use java6 for compatibility.

--- src/test/java/it/ciscosistem/actormodel/TestMavenProjectStub.java
+++ src/test/java/it/ciscosistem/actormodel/TestMavenProjectStub.java
@@ -9,7 +9,7 @@ import org.apache.maven.plugin.testing.stubs.MavenProjectStub;
 public class TestMavenProjectStub extends MavenProjectStub {
     @Override
     public List<String> getRuntimeClasspathElements() throws DependencyResolutionRequiredException {
-        List<String> runtimeClassPathElements = new ArrayList<>();
+        List<String> runtimeClassPathElements = new ArrayList<String>();
         runtimeClassPathElements.add("it.ciscosistem.actormodel.java.model.Fuu");
         runtimeClassPathElements.add("it.ciscosistem.actormodel.java.model.FuuChild1");
         runtimeClassPathElements.add("it.ciscosistem.actormodel.java.model.FuuChild2");
